03b0a7c7934064eccea99be61204baa46a7a3285
JohannHelbert <>
2024-01-10 17:41:50 +0000

    change function

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = MultiPage(app_name="Mildew Detection")  # Create an instance of the app
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaves Visualiser", page_leaves_visualizer_body)
-app.add_page("Powdery Mildew detector", page_powdery_mildew_detector_body)
+app.add_page("Powdery Mildew detector", page_mildew_detector_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
